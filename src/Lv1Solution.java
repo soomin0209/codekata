6f592a1c3e29186c487fc34cec70fcdcff6c1e90
soomin0209 <soomin02094@gmail.com>
2026-01-21 11:39:36 +0900

    8. 각도기

--- src/Lv1Solution.java
+++ src/Lv1Solution.java
@@ -41,4 +41,19 @@ public class Lv1Solution {
         int answer = (int)(((double)num1 / (double)num2) * 1000);
         return answer;
     }
+
+    // 8. 각도기
+    public int solution8(int angle) {
+        int answer = 0;
+        if (0 < angle && angle < 90) {
+            answer = 1;
+        } else if (angle == 90) {
+            answer = 2;
+        } else if (90 < angle && angle < 180) {
+            answer = 3;
+        } else if (angle == 180) {
+            answer = 4;
+        }
+        return answer;
+    }
 }
