4a8923d660dae60c5ed00567cb09ddb903d988f7
soomin0209 <soomin02094@gmail.com>
2026-01-21 11:40:17 +0900

    9. 짝수의 합

--- src/Lv1Solution.java
+++ src/Lv1Solution.java
@@ -56,4 +56,15 @@ public class Lv1Solution {
         }
         return answer;
     }
+
+    // 9. 짝수의 합
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 0; i <= n; i++) {
+            if (i % 2 == 0) {
+                answer += i;
+            }
+        }
+        return answer;
+    }
 }
