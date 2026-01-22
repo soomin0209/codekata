5fe148681f8b791506bdbbbfaed72f793450835c
soomin0209 <soomin02094@gmail.com>
2026-01-22 10:18:10 +0900

    16. x만큼 간격이 있는 n개의 숫자

--- src/Lv2Solution.java
+++ src/Lv2Solution.java
@@ -52,4 +52,13 @@ public class Lv2Solution {
         }
         return answer;
     }
+
+    // 16. x만큼 간격이 있는 n개의 숫자
+    public long[] solution16(int x, int n) {
+        long[] answer = new long[n];
+        for (int i = 0; i < n; i++) {
+            answer[i] = (i + 1) * (long)x;
+        }
+        return answer;
+    }
 }
