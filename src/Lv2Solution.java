053b97ec36490dc9a459f27c66e35955ca53b865
Soomin <soomin02094@gmail.com>
2026-02-03 09:53:09 +0900

    30. 가운데 글자 가져오기

--- src/Lv2Solution.java
+++ src/Lv2Solution.java
@@ -303,4 +303,16 @@ public class Lv2Solution {
         }
         return answer;
     }
+
+    // 30. 가운데 글자 가져오기
+    public String solution30(String s) {
+        String answer;
+        int mid = s.length() / 2;
+        if (s.length() % 2 == 0) {
+            answer = s.substring(mid - 1, mid + 1);
+        } else {
+            answer = s.substring(mid, mid + 1);
+        }
+        return answer;
+    }
 }
