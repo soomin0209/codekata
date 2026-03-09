ca119e6a42179edf7ed607b00dde4b483049e72d
Soomin <soomin02094@gmail.com>
2026-03-09 09:11:47 +0900

    43. 크기가 작은 부분 문자열

--- src/Lv3Solution.java
+++ src/Lv3Solution.java
@@ -39,4 +39,18 @@ public class Lv3Solution {
         }
         return count;
     }
+
+    // 43. 크기가 작은 부분 문자열
+    public int solution43(String t, String p) {
+        int answer = 0;
+
+        for (int i = 0; i <= t.length() - p.length(); i++) {
+            String portion = t.substring(i, i + p.length());
+            if (portion.compareTo(p) <= 0) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
 }
